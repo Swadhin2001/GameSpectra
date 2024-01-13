24b865b2b63727242a13e9a81dcb7ea7199fb80a
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-01-13 13:01:24 +0530

    bug fixed

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['media.rawg.io'],
-},
-}
+    domains: ["media.rawg.io"],
+  },
+  serverRuntimeConfig: {
+    API_URL: process.env.NEXT_PUBLIC_APIKEY,
+  },
+  // Will be available on both server and client
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_APIKEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
